0ad86538fbb762e90ee6bb42445668761d2d4904
alireza <alirezaariye@gmail.com>
2023-05-28 14:44:38 +0330

    feat: add login_screen & home_screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/dogdom_login.png'),
+            fit: BoxFit.cover
+          ),
+        ),
+
+      ),
+    );
+  }
+}
