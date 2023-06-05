5fb3dc37cdd8722f275e3f54b353981f2bda3311
alireza <alirezaariye@gmail.com>
2023-06-05 22:09:35 +0330

    feat: add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dogdom/screens/login_screen.dart';
+import 'package:dogdom/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: Login(),
+      home: Splash(),
     );
   }
 }
